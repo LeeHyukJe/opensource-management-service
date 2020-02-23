ab104c237d55666c5fc4d31e6d1392245a0520b7
leehyukje <lims2733@naver.com>
2020-02-23 23:16:10 +0900

    bridge the communication

--- tests/e2e/.eslintrc.js
+++ tests/e2e/.eslintrc.js
@@ -1,5 +1,19 @@
 module.exports = {
-  rules: {
-    'no-unused-expressions': 'off'
-  }
-}
+    "extends": "standard",
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-trailing-spaces": 0,
+        "keyword-spacing": 0,
+        "no-unused-vars": 1,
+        "no-multiple-empty-lines": 0,
+        "space-before-function-paren": 0,
+        "eol-last": 0
+    }
+};
